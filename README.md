c3c776572256df6ae2fb6f83d0e754d797712062
Kevin Huang <kevin.huang23@ycdsbk12.ca>
2022-01-24 12:21:20 -0500

    Working on random generation of the map

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-f059dc9a6f8d3a56e377f745f24479a46679e63a5d9fe6f495e02850cd0d8118.svg)](https://classroom.github.com/online_ide?assignment_repo_id=6696052&assignment_repo_type=AssignmentRepo)
 # Program Title (edit this)
 
-Put your program description here.
+Game: (Undecided)
+
+
+Description:
+
+
+Implementation:
+This game sets up a map using a nested for loop that iterates through a 2d array. It takes 
--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,75 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+  // Map is made using a tile system with a 2d array, if the 2d array is true, then it will trigger states to stop player from moving there
+  public int xTile = 20;
+  public int yTile = 20; 
+  boolean[][] mapCoord = new boolean[xTile][yTile];
+  public float xTileSize = width / xTile;
+  public float yTileSize = height / yTile;
+  // Each individual tile equals 40x40 pixels
 	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+ 
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
+
   public void setup() {
     background(210, 255, 173);
+
+    for (int row = 0; row < xTile; row++) {
+      for (int column = 0; column < yTile; column++) {
+        if (random(1) < .25) {
+          mapCoord[xTile][yTile] = true;
+        }
+      }
+    }
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  
+    for (int row = 0; row < xTile; row++) {
+      for (int column = 0; column < yTile; column++) {
+        
+        float xTileLocation = xTileSize * column;
+        float yTileLocation = yTileSize * row;
+
+        if (mapCoord[row][column]) {
+          fill(95, 60, 25);
+          rect(xTileLocation, yTileLocation, 5, 10);
+          fill(60, 100, 20);
+          noStroke();
+          triangle(xTileLocation, yTileLocation, xTileLocation + 10, yTileLocation + 10, xTileLocation + 20, yTileLocation);
+          triangle(xTileLocation, yTileLocation + 5, xTileLocation + 10, yTileLocation + 15, xTileLocation + 20, yTileLocation + 5);
+          triangle(xTileLocation, yTileLocation + 10, xTileLocation + 10, yTileLocation + 20, xTileLocation + 20, yTileLocation + 10);
+
+          stroke(0);
+          rect(xTileLocation, yTileLocation, xTileSize, yTileSize);
+        }
+
+      }
+    }
+
+    // Button for refreshing the map
+    
   }
   
   // define other methods down here.
+// Left edge at rectX, top edge at rectY, right edge at rectX + rectWidth, bottom edge at rectY + rectHeight
+  // public int collisionDetect(int ) {
+    // if(collision > rectX && collision < rectX + rectWidth && collision > rectY && collision < rectY + rectHeight){
+
+  // }
+  /*
+  public void mouseClicked() {
+    if (mouseClicked )
+  }
+  */
 }
